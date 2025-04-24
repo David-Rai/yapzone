a90903df43db9acb4d4d2b07cd7dc78d54624b7b
david-rai <davidkhaling123@gmail.com>
2025-04-24 14:22:06 +0545

    finally the button added

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,5 +1,16 @@
 // demo.js
-setInterval(() => {
-    console.log("sdaad")
-    document.title = "adasdasda";
-  }, 2000);
+setTimeout(()=>{
+    document.title = "yapzone"
+    alert("bro")
+    
+const btn=document.createElement("button")
+btn.id="yapzonebutton"
+btn.innerText="clicksadhsdfhaskdfkfgsakdfgahsdfghadsgfksdajgfasdufgsdayufgdaskfgsdahfvsdakadsjgsdahsakd here"
+btn.style.position="absolute"
+btn.style.top="0"
+btn.style.right="0"
+
+console.log(btn)
+document.body.appendChild(btn)
+},5000)
+
