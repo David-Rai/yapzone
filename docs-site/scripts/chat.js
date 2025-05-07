418364d90c72676377e36b2c1d52e571af297428
david-rai <davidkhaling123@gmail.com>
2025-05-07 22:41:39 +0545

    day14:toggeling live chat done

--- docs-site/scripts/chat.js
+++ docs-site/scripts/chat.js
@@ -1,7 +1,13 @@
 const btn=document.querySelector("#chat")
+const btn2=document.querySelector(".live-cross")
 const live=document.querySelector(".live-chat")
 
 btn.addEventListener("click",()=>{
+    // alert("hey there")
+    live.classList.toggle("showLive")
+})
+
+btn2.addEventListener("click",()=>{
     // alert("hey there")
     live.classList.toggle("showLive")
 })
